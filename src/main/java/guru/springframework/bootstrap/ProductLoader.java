be1ad13be67387e28c9c359ffc9625edc2b58124
Junlapong L <junlapong@gmail.com>
2016-11-28 02:59:19 +0700

    first commit

--- src/main/java/guru/springframework/bootstrap/ProductLoader.java
+++ src/main/java/guru/springframework/bootstrap/ProductLoader.java
@@ -2,7 +2,6 @@ package guru.springframework.bootstrap;
 
 import guru.springframework.domain.Product;
 import guru.springframework.repositories.ProductRepository;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -10,12 +9,15 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Component
 public class ProductLoader implements ApplicationListener<ContextRefreshedEvent> {
 
-    private ProductRepository productRepository;
+    private static final Logger log = LoggerFactory.getLogger(ProductLoader.class);
 
-    private Logger log = Logger.getLogger(ProductLoader.class);
+    private ProductRepository productRepository;
 
     @Autowired
     public void setProductRepository(ProductRepository productRepository) {
@@ -32,7 +34,7 @@ public class ProductLoader implements ApplicationListener<ContextRefreshedEvent>
         shirt.setProductId("235268845711068308");
         productRepository.save(shirt);
 
-        log.info("Saved Shirt - id: " + shirt.getId());
+        log.info("Saved Shirt - id: {}", shirt.getId());
 
         Product mug = new Product();
         mug.setDescription("Spring Framework Guru Mug");
@@ -41,6 +43,6 @@ public class ProductLoader implements ApplicationListener<ContextRefreshedEvent>
         mug.setPrice(new BigDecimal("11.95"));
         productRepository.save(mug);
 
-        log.info("Saved Mug - id:" + mug.getId());
+        log.info("Saved Mug - id: {}", mug.getId());
     }
 }
